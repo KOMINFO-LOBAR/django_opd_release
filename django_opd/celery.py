90a61e9e5d92dd8bf03d45f0f9928ad28bc00ebf
manjaro-linux <suratiwan03@gmail.com>
2023-12-22 16:16:10 +0800

    add time zone to celery

--- django_opd/celery.py
+++ django_opd/celery.py
@@ -7,4 +7,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','django_opd.settings')
 app=Celery('django_opd')
 app.config_from_object('django.conf:settings',namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.timezone='Asia/Makassar'
 app.conf.beat_schedule={'periodic-clear-sessions':{_A:'opd.tasks.session_cleanup',_B:crontab(minute=0,hour=0)},'periodic-summary-hitcount':{_A:'opd.tasks.summary_hitcount',_B:crontab(minute=0,hour=1)},'periodic-get-ip-location':{_A:'opd.tasks.geo_location_hitcount',_B:crontab(minute=0,hour='*/6')}}
