8c2682b40ac32d19fb3f258a00a74eed1afe4bc8
Ione03 <suratiwan03@gmail.com>
2024-09-18 14:57:21 +0800

    Recreate Site Data

--- opd/management/commands/getsitedatadwp.py
+++ opd/management/commands/getsitedatadwp.py
@@ -1,11 +1,12 @@
 _H='thumbnail'
 _G='nama'
 _F='url'
-_E='Asia/Makassar'
-_D='slug'
+_E='slug'
+_D='Asia/Makassar'
 _C='name'
 _B='updated_at'
 _A='created_at'
+import sys
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.db import connections
@@ -16,8 +17,8 @@ from django.db import transaction
 import pytz
 class Command(BaseCommand):
 	help='Get Site Data Specific for DWP'
-	def info(self,message):self.stdout.write(message)
-	def debug(self,message):self.stdout.write(message)
+	def info(self,message):sys.stdout.write(message)
+	def debug(self,message):sys.stdout.write(message)
 	@transaction.atomic
 	def create_site(self):obj,created=Site.objects.get_or_create(domain='dwp.lombokbaratkab.go.id',defaults={_C:'DWP'});return obj
 	@transaction.atomic
@@ -33,23 +34,23 @@ class Command(BaseCommand):
 			cursor.execute(sql_string);dat=cursor.fetchall();desc=cursor.description;data_dic=[dict(zip([col[0]for col in desc],row))for row in dat];return data_dic
 	@transaction.atomic
 	def create_instansi(self,site,user):
-		A='email';timezone=pytz.timezone(_E);table_name='app_settings';app_settings=self.get_data(table_name);mlist=[]
+		A='email';timezone=pytz.timezone(_D);table_name='app_settings';app_settings=self.get_data(table_name);mlist=[]
 		for i in range(len(app_settings)):tmp=app_settings[i];condition={_G:tmp[_C],'site':site};defaults={};defaults['alamat']=tmp['address'];defaults['telp']=tmp['phone'];defaults[A]=tmp[A];defaults[_A]=timezone.localize(tmp[_A]);defaults[_B]=timezone.localize(tmp[_B]);obj,created=instansi.objects.update_or_create(nama=tmp[_C],site=site,defaults=defaults);obj.admin.add(user);obj.save();instansi.objects.filter(nama=tmp[_C],site=site).update(created_at=timezone.localize(tmp[_A]))
 	@transaction.atomic
 	def create_banner(self,site):
-		A='position';timezone=pytz.timezone(_E);table_name='banners';app_model=self.get_data(table_name);mlist=[]
+		A='position';timezone=pytz.timezone(_D);table_name='banners';app_model=self.get_data(table_name);mlist=[]
 		for i in range(len(app_model)):
 			tmp=app_model[i]
 			if tmp[A]=='top':obj_photo,created=photo.objects.update_or_create(site=site,jenis=photo.Jenis.BANNER_TOP,defaults={'file_path':tmp[_H]});obj,created=banner.objects.update_or_create(position=tmp[A],site=site,defaults={'link':tmp[_F],'photo':obj_photo,_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])});banner.objects.filter(position=tmp[A],site=site).update(created_at=timezone.localize(tmp[_A]))
 	@transaction.atomic
 	def create_kategori(self,site):
-		timezone=pytz.timezone(_E);table_name='categories';app_model=self.get_data(table_name);mlist=[]
-		for i in range(len(app_model)):tmp=app_model[i];obj,created=kategori.objects.update_or_create(slug=tmp[_D],site__id=site.id,defaults={_G:tmp[_C],_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])});obj.site.add(site);obj.save();kategori.objects.filter(slug=tmp[_D],site__id=site.id).update(created_at=timezone.localize(tmp[_A]));obj_tag,created=tags.objects.update_or_create(nama=tmp[_C],site__id=site.id,defaults={_D:tmp[_D],_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])});obj_tag.site.add(site);obj_tag.save();tags.objects.filter(nama=tmp[_C],site__id=site.id).update(created_at=timezone.localize(tmp[_A]))
+		timezone=pytz.timezone(_D);table_name='categories';app_model=self.get_data(table_name);mlist=[]
+		for i in range(len(app_model)):tmp=app_model[i];obj,created=kategori.objects.update_or_create(slug=tmp[_E],site__id=site.id,defaults={_G:tmp[_C],_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])});obj.site.add(site);obj.save();kategori.objects.filter(slug=tmp[_E],site__id=site.id).update(created_at=timezone.localize(tmp[_A]));obj_tag,created=tags.objects.update_or_create(nama=tmp[_C],site__id=site.id,defaults={_E:tmp[_E],_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])});obj_tag.site.add(site);obj_tag.save();tags.objects.filter(nama=tmp[_C],site__id=site.id).update(created_at=timezone.localize(tmp[_A]))
 	@transaction.atomic
 	def create_berita(self,site,user):
-		timezone=pytz.timezone(_E);table_name='news';app_model=self.get_data(table_name);cat_news_default,created=kategori.objects.get_or_create(slug='berita-utama',site__id=site.id,defaults={_G:'Berita Utama'});mlist=[]
-		for i in range(len(app_model)):
-			tmp=app_model[i];obj_photo,created=photo.objects.get_or_create(site_id=site.id,jenis=photo.Jenis.HIGHLIGHT1,file_path=tmp[_H]);sql_='SELECT news.id, news.title, category_news.category_id, categories.name FROM  ';sql_=sql_+'news INNER JOIN  category_news ON news.id = category_news.news_id INNER JOIN ';sql_=sql_+'categories ON category_news.category_id = categories.id WHERE news.slug=';sql_=sql_+'"'+str(tmp[_D])+'"';sql_=sql_+' order by news.id;';app_model_cat=self.get_data(table_name=None,opt_query=sql_);cat_name=[]
+		timezone=pytz.timezone(_D);table_name='news';app_model=self.get_data(table_name);cat_news_default,created=kategori.objects.get_or_create(slug='berita-utama',site__id=site.id,defaults={_G:'Berita Utama'});self.info('\n');mlist=[];lendata=len(app_model)
+		for i in range(lendata):
+			tmp=app_model[i];tmp_count=(i+1)/lendata;time_msg='\rWriting Progress [Data] at {0:.2%} '.format(tmp_count);self.info(time_msg);obj_photo,created=photo.objects.get_or_create(site_id=site.id,jenis=photo.Jenis.HIGHLIGHT1,file_path=tmp[_H]);sql_='SELECT news.id, news.title, category_news.category_id, categories.name FROM  ';sql_=sql_+'news INNER JOIN  category_news ON news.id = category_news.news_id INNER JOIN ';sql_=sql_+'categories ON category_news.category_id = categories.id WHERE news.slug=';sql_=sql_+'"'+str(tmp[_E])+'"';sql_=sql_+' order by news.id;';app_model_cat=self.get_data(table_name=None,opt_query=sql_);cat_name=[]
 			for j in range(len(app_model_cat)):tmp_cat=app_model_cat[j];cat_name.append(tmp_cat[_C])
 			m_status=Status.DRAFT
 			if tmp['drafted']=='Tidak':m_status=Status.PUBLISHED
@@ -57,17 +58,20 @@ class Command(BaseCommand):
 			if len(cat_name)>0:
 				cat_name_0=kategori.objects.filter(site__id=site.id,nama=cat_name[0])[:1]
 				if cat_name_0:cat_name_0=cat_name_0.get()
-			obj,created=berita.objects.update_or_create(judul_seo=tmp[_D],site_id=site.id,defaults={'judul':tmp['title'],'status':m_status,'admin_id':user.id,'kategori_id':cat_name_0.id if cat_name_0 else cat_news_default.id,'isi_berita':tmp['content'],'view_count':tmp['viewed'],_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])});obj.photo.clear();obj.photo.add(obj_photo);obj.tags.clear()
+			obj,created=berita.objects.update_or_create(judul=tmp['title'],site_id=site.id,defaults={'judul_seo':tmp[_E],'status':m_status,'admin_id':user.id,'kategori_id':cat_name_0.id if cat_name_0 else cat_news_default.id,'isi_berita':tmp['content'],'view_count':tmp['viewed'],_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])})
+			if created:pass
+			else:pass
+			obj.photo.clear();obj.photo.add(obj_photo);obj.tags.clear()
 			for j in range(len(cat_name)):
 				cat_name_0=tags.objects.filter(site__id=site.id,nama=cat_name[j])[:1]
 				if cat_name_0:cat_name_0=cat_name_0.get();obj.tags.add(cat_name_0)
-			obj.save();obj=berita.objects.filter(judul_seo=tmp[_D],site_id=site.id);obj.update(created_at=timezone.localize(tmp[_A]));obj.update(judul_seo=tmp[_D])
+			obj.save();obj=berita.objects.filter(id=obj.id);obj.update(created_at=timezone.localize(tmp[_A]));
 	@transaction.atomic
 	def create_related_link(self,site):
-		timezone=pytz.timezone(_E);table_name='related_links';app_model=self.get_data(table_name)
+		timezone=pytz.timezone(_D);table_name='related_links';app_model=self.get_data(table_name)
 		for i in range(len(app_model)):tmp=app_model[i];obj,created=link_terkait.objects.update_or_create(site__id=site.id,link=tmp[_F],defaults={_G:tmp[_C],_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])});obj.site.add(site);obj.save();obj=link_terkait.objects.filter(site__id=site.id,link=tmp[_F]);obj.update(created_at=timezone.localize(tmp[_A]))
 	@transaction.atomic
 	def create_social_media(self,site):
-		timezone=pytz.timezone(_E);table_name='social_medias';app_model=self.get_data(table_name)
+		timezone=pytz.timezone(_D);table_name='social_medias';app_model=self.get_data(table_name)
 		for i in range(len(app_model)):tmp=app_model[i];obj,created=social_media.objects.update_or_create(site_id=site.id,link=tmp[_F],defaults={'jenis':tmp[_C],_A:timezone.localize(tmp[_A]),_B:timezone.localize(tmp[_B])});obj=social_media.objects.filter(site_id=site.id,link=tmp[_F]);obj.update(created_at=timezone.localize(tmp[_A]))
-	def handle(self,*args,**options):self.info('---Begin get DWP Data---');self.info('Create Site');site=self.create_site();self.info('Create User');user=self.create_user();self.info('Create Instansi');self.create_instansi(site,user);self.info('Create Banner');self.create_banner(site);self.info('Create Categories');self.create_kategori(site);self.info('Create News');self.create_berita(site,user);self.info('Create Related Link');self.create_related_link(site);self.info('Create Social Media');self.create_social_media(site);self.info('---End get DWP Data---')
+	def handle(self,*args,**options):self.info('\n---Begin get DWP Data---');self.info('\nCreate Site');site=self.create_site();self.info('\nCreate User');user=self.create_user();self.info('\nCreate Instansi');self.create_instansi(site,user);self.info('\nCreate Banner');self.create_banner(site);self.info('\nCreate Categories');self.create_kategori(site);self.info('\nCreate News');self.create_berita(site,user);self.info('\nCreate Related Link');self.create_related_link(site);self.info('\nCreate Social Media');self.create_social_media(site);self.info('\n---End get DWP Data---\n')
--- opd/management/commands/updatesitedata.py
+++ opd/management/commands/updatesitedata.py
@@ -1,23 +1,13 @@
-_S='proses model: '
-_R='Update Site Data ...'
-_Q='site_group'
-_P='instansi'
-_O='SKIP'
-_N='halaman_statis'
-_M='menu_id'
-_L='admin'
-_K='photo'
-_J='tags'
-_I='photo_id'
-_H='site'
-_G=True
-_F=False
-_E='r'
-_D='m2m'
+_I='site_group'
+_H='halaman_statis'
+_G='instansi'
+_F='site'
+_E='m2m'
+_D='r'
 _C='id'
 _B='defaults'
 _A='condition'
-import os
+import sys,os
 from django.core.management.base import BaseCommand
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
@@ -28,158 +18,158 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.db import transaction
 from opd.models import kategori,photo,tags,menu
 class Command(BaseCommand):
-	help='Detach all table base on site id';base_dir=settings.BASE_DIR;site_list=os.path.join(base_dir,_H);photo_dir=os.path.join(base_dir,'media','images');model_list=['berita','pengumuman','artikel','dokumen','link_terkait','galery_foto','galery_layanan','galery_video','instansi_kategori',_P,'social_media','logo','banner','pejabat',_N,'agenda','popup','info_hoax','info_widget','banner_all']
-	def info(A,message):A.stdout.write(message);A.stdout.flush()
-	def debug(A,message):A.stdout.write(message);A.stdout.flush()
-	def add_arguments(A,parser):parser.add_argument(_Q,type=str)
+	help='Detach all table base on site id';base_dir=settings.BASE_DIR;site_list=os.path.join(base_dir,_F);photo_dir=os.path.join(base_dir,'media','images');model_list=['berita','pengumuman','artikel','dokumen','link_terkait','galery_foto','galery_layanan','galery_video','instansi_kategori',_G,'social_media','logo','banner','pejabat',_H,'agenda','popup','info_hoax','info_widget','banner_all']
+	def info(A,message):sys.stdout.write(message)
+	def debug(A,message):sys.stdout.write(message)
+	def add_arguments(A,parser):parser.add_argument(_I,type=str)
 	@transaction.atomic
 	def update_site_model(self,site_group):
-		with open(os.path.join(self.site_list,site_group,'Site.json'),_E)as D:A=json.load(D)
-		B=1;E=len(A)
-		for C in A:F=B/E;G='\rWriting Progress [Site] at {0:.2%}\r'.format(F);self.info(G);H,I=Site.objects.update_or_create(id=C[_A][_C],defaults=C[_B]);B+=1
+		A=self;A.info('\nUpdate Site Model ...')
+		with open(os.path.join(A.site_list,site_group,'Site.json'),_D)as E:B=json.load(E)
+		C=1;F=len(B)
+		for D in B:G=C/F;H='\rWriting Progress [Site] at {0:.2%}\r'.format(G);A.info(H);I,J=Site.objects.update_or_create(id=D[_A][_C],defaults=D[_B]);C+=1
 	@transaction.atomic
 	def update_user_model(self,site_group):
-		with open(os.path.join(self.site_list,site_group,'User.json'),_E)as D:A=json.load(D)
-		B=1;E=len(A)
-		for C in A:F=B/E;G='\rWriting Progress [User] at {0:.2%} '.format(F);self.info(G);H,I=User.objects.update_or_create(id=C[_A][_C],defaults=C[_B]);B+=1
+		A=self;A.info('\nUpdate User Model ...')
+		with open(os.path.join(A.site_list,site_group,'User.json'),_D)as E:B=json.load(E)
+		C=1;F=len(B)
+		for D in B:G=C/F;H='\rWriting Progress [User] at {0:.2%} '.format(G);A.info(H);I,J=User.objects.update_or_create(id=D[_A][_C],defaults=D[_B]);C+=1
 	@transaction.atomic
 	def update_photo_model(self,site_group):
-		with open(os.path.join(self.site_list,site_group,'photo.json'),_E)as D:A=json.load(D)
-		B=1;E=len(A)
-		for C in A:F=B/E;G='\rWriting Progress [Photo] at {0:.2%} '.format(F);self.info(G);H,I=photo.objects.update_or_create(id=C[_A][_C],defaults=C[_B]);B+=1
+		A=self;A.info('\nUpdate Photo Model ...')
+		with open(os.path.join(A.site_list,site_group,'photo.json'),_D)as E:B=json.load(E)
+		C=1;F=len(B)
+		for D in B:G=C/F;H='\rWriting Progress [Photo] at {0:.2%} '.format(G);A.info(H);I,J=photo.objects.update_or_create(id=D[_A][_C],defaults=D[_B]);C+=1
 	@transaction.atomic
 	def update_kategori_model(self,site_group):
-		with open(os.path.join(self.site_list,site_group,'kategori.json'),_E)as D:A=json.load(D)
-		B=1;E=len(A)
-		for C in A:F=B/E;G='\rWriting Progress [Kategori] at {0:.2%} '.format(F);self.info(G);H,I=kategori.objects.update_or_create(id=C[_A][_C],defaults=C[_B]);B+=1
+		A=self;A.info('\nUpdate Kategori Model ...')
+		with open(os.path.join(A.site_list,site_group,'kategori.json'),_D)as E:B=json.load(E)
+		C=1;F=len(B)
+		for D in B:G=C/F;H='\rWriting Progress [Kategori] at {0:.2%} '.format(G);A.info(H);I,J=kategori.objects.update_or_create(id=D[_A][_C],defaults=D[_B]);C+=1
 	@transaction.atomic
 	def update_tags_model(self,site_group):
-		with open(os.path.join(self.site_list,site_group,'tags.json'),_E)as D:A=json.load(D)
-		B=1;E=len(A)
-		for C in A:F=B/E;G='\rWriting Progress [Tags] at {0:.2%} '.format(F);self.info(G);H,I=tags.objects.update_or_create(id=C[_A][_C],defaults=C[_B]);B+=1
+		A=self;A.info('\nUpdate Tags Model ...')
+		with open(os.path.join(A.site_list,site_group,'tags.json'),_D)as E:B=json.load(E)
+		C=1;F=len(B)
+		for D in B:G=C/F;H='\rWriting Progress [Tags] at {0:.2%} '.format(G);A.info(H);I,J=tags.objects.update_or_create(id=D[_A][_C],defaults=D[_B]);C+=1
 	@transaction.atomic
 	def update_menu_model(self,site_group):
-		with open(os.path.join(self.site_list,site_group,'menu.json'),_E)as E:C=json.load(E)
-		D=1;F=len(C)
-		for A in C:
-			G=D/F;H='\rWriting Progress [Menu] at {0:.2%} '.format(G);self.info(H);B,K=menu.objects.update_or_create(id=A[_A][_C],defaults=A[_B]);B.site.clear()
-			for I in A[_D][_H]:J=Site.objects.get(id=I);B.site.add(J)
-			B.save();D+=1
+		A=self;A.info('\nUpdate Menu Model ...')
+		with open(os.path.join(A.site_list,site_group,'menu.json'),_D)as F:D=json.load(F)
+		E=1;G=len(D)
+		for B in D:
+			H=E/G;I='\rWriting Progress [Menu] at {0:.2%} '.format(H);A.info(I);C,L=menu.objects.update_or_create(id=B[_A][_C],defaults=B[_B]);C.site.clear()
+			for J in B[_E][_F]:K=Site.objects.get(id=J);C.site.add(K)
+			C.save();E+=1
 	@transaction.atomic
 	def update_data_by_site(self,site_group):
-		S=site_group;P='created_at';J=self;I='nama_seo';H='slug';F='judul_seo';T=os.path.join(J.site_list,S);Q=[A for A in os.listdir(T)if os.path.isfile(os.path.join(T,A))]
-		for K in J.model_list:
-			for D in range(len(Q)):
-				L=Q[D].split('_');N=L[len(L)-1];N=N.split('.')[:1];N=N[0];L.pop(len(L)-1);a=0;b='_'.join(L)
-				if K==b:
-					m=None
-					with open(os.path.join(J.site_list,S,Q[D]),_E)as c:U=json.load(c)
-					V=1;d=len(U)
-					for A in U:
+		h='photo_id';g='menu_id';f='admin';e='photo';d='tags';U=site_group;R='created_at';Q=True;P=False;H=self;G='nama_seo';F='slug';E='judul_seo';H.info('\nUpdate Data By Site ...');V=os.path.join(H.site_list,U);S=[A for A in os.listdir(V)if os.path.isfile(os.path.join(V,A))]
+		for K in H.model_list:
+			H.info('\nproses model: '+K+'\n')
+			for I in range(len(S)):
+				L=S[I].split('_');M=L[len(L)-1];M=M.split('.')[:1];M=M[0];L.pop(len(L)-1);W=0;i='_'.join(L)
+				if K==i:
+					t=None
+					with open(os.path.join(H.site_list,U,S[I]),_D)as j:X=json.load(j)
+					Y=1;k=len(X)
+					for A in X:
 						C=apps.get_model('opd',K)
 						if C:
-							O=0;M=[]
-							for E in C._meta.fields:M.append(E.name)
-							if _H in M:O=1
+							O=0;N=[]
+							for J in C._meta.fields:N.append(J.name)
+							if _F in N:O=1
 							if O==0:
-								M=[]
-								for E in C._meta.get_fields():M.append(E.name)
-								if _H in M:O=2
-							W=_F;X=_F;Y=_F;Z=_F
-							for E in C._meta.get_fields():
-								if E.name==F:Z=_G
-								if E.many_to_many:
-									if E.name==_J:W=_G
-									elif E.name==_K:X=_G
-									elif E.name==_L:Y=_G
-							if K==_N:
-								e=menu.objects.filter(id=A[_B][_M])
-								if not e:a+=1;continue
-							if K==_P:A[_B].pop('parent_id',None)
-							f=V/d;g='\rWriting Progress [Data] at {0:.2%} '.format(f);J.info(g)
-							if _I in A[_B]:
-								R=photo.objects.filter(id=A[_B][_I])
-								if R:
-									if H in A[_A]:B,G=C.objects.update_or_create(slug=A[_A][H],defaults=A[_B])
-									elif F in A[_A]:B,G=C.objects.update_or_create(judul_seo=A[_A][F],defaults=A[_B])
-									elif I in A[_A]:B,G=C.objects.update_or_create(nama_seo=A[_A][I],defaults=A[_B])
-									else:B,G=C.objects.update_or_create(id=A[_A][_C],defaults=A[_B])
-							elif H in A[_A]:B,G=C.objects.update_or_create(slug=A[_A][H],defaults=A[_B])
-							elif F in A[_A]:B,G=C.objects.update_or_create(judul_seo=A[_A][F],defaults=A[_B])
-							elif I in A[_A]:B,G=C.objects.update_or_create(nama_seo=A[_A][I],defaults=A[_B])
-							else:B,G=C.objects.update_or_create(id=A[_A][_C],defaults=A[_B])
-							if W:
+								N=[]
+								for J in C._meta.get_fields():N.append(J.name)
+								if _F in N:O=2
+							Z=P;a=P;b=P;c=P
+							for J in C._meta.get_fields():
+								if J.name==E:c=Q
+								if J.many_to_many:
+									if J.name==d:Z=Q
+									elif J.name==e:a=Q
+									elif J.name==f:b=Q
+							if K==_H:
+								l=menu.objects.filter(id=A[_B][g])
+								if not l:W+=1;H.info('\nSKIP',A[_B][g]);continue
+							if K==_G:A[_B].pop('parent_id',None)
+							m=Y/k;n='\rWriting Progress [Data Site ID '+str(M)+'] at {0:.2%} '.format(m);H.info(n)
+							if h in A[_B]:
+								T=photo.objects.filter(id=A[_B][h])
+								if T:
+									if F in A[_A]:
+										B=C.objects.filter(slug=A[_A][F])
+										if B:B,D=C.objects.update_or_create(slug=A[_A][F],defaults=A[_B])
+										else:B=C.objects.filter(id=A[_B][_C])
+										if not B:A[_B].pop(_C);B,D=C.objects.update_or_create(slug=A[_A][F],defaults=A[_B])
+										else:B=B.get()
+									elif E in A[_A]:
+										B=C.objects.filter(judul_seo=A[_A][E])
+										if B:B,D=C.objects.update_or_create(judul_seo=A[_A][E],defaults=A[_B])
+										else:
+											B=C.objects.filter(id=A[_B][_C])
+											if not B:A[_B].pop(_C);B,D=C.objects.update_or_create(judul_seo=A[_A][E],defaults=A[_B])
+											else:B=B.get()
+									elif G in A[_A]:
+										B=C.objects.filter(nama_seo=A[_A][G])
+										if B:B,D=C.objects.update_or_create(nama_seo=A[_A][G],defaults=A[_B])
+										else:
+											B=C.objects.filter(id=A[_B][_C])
+											if not B:A[_B].pop(_C);B,D=C.objects.update_or_create(nama_seo=A[_A][G],defaults=A[_B])
+											else:B=B.get()
+									else:B,D=C.objects.update_or_create(id=A[_A][_C],defaults=A[_B])
+							elif F in A[_A]:
+								B=C.objects.filter(slug=A[_A][F])
+								if B:B,D=C.objects.update_or_create(slug=A[_A][F],defaults=A[_B])
+								else:
+									B=C.objects.filter(id=A[_B][_C])
+									if not B:A[_B].pop(_C);B,D=C.objects.update_or_create(slug=A[_A][F],defaults=A[_B])
+									else:B=B.get()
+							elif E in A[_A]:
+								B=C.objects.filter(judul_seo=A[_A][E])
+								if B:B,D=C.objects.update_or_create(judul_seo=A[_A][E],defaults=A[_B])
+								else:
+									B=C.objects.filter(id=A[_B][_C])
+									if not B:A[_B].pop(_C);B,D=C.objects.update_or_create(judul_seo=A[_A][E],defaults=A[_B])
+									else:B=B.get()
+							elif G in A[_A]:
+								B=C.objects.filter(nama_seo=A[_A][G])
+								if B:B,D=C.objects.update_or_create(nama_seo=A[_A][G],defaults=A[_B])
+								else:
+									B=C.objects.filter(id=A[_B][_C])
+									if not B:A[_B].pop(_C);B,D=C.objects.update_or_create(nama_seo=A[_A][G],defaults=A[_B])
+									else:B=B.get()
+							else:B,D=C.objects.update_or_create(id=A[_A][_C],defaults=A[_B])
+							if Z:
 								B.tags.clear()
-								for D in A[_D][_J]:h=tags.objects.get(id=D);B.tags.add(h)
+								for I in A[_E][d]:o=tags.objects.get(id=I);B.tags.add(o)
 								B.save()
-							if X:
+							if a:
 								B.photo.clear()
-								for D in A[_D][_K]:
-									R=photo.objects.filter(id=D)
-									if R:i=photo.objects.get(id=D);B.photo.add(i)
+								for I in A[_E][e]:
+									T=photo.objects.filter(id=I)
+									if T:p=photo.objects.get(id=I);B.photo.add(p)
 								B.save()
-							if Y:
+							if b:
 								B.admin.clear()
-								for D in A[_D][_L]:j=User.objects.get(id=D);B.admin.add(j)
+								for I in A[_E][f]:q=User.objects.get(id=I);B.admin.add(q)
 								B.save()
 							if O==2:
-								if _D in A:
-									if _H in A[_D]:
+								if _E in A:
+									if _F in A[_E]:
 										B.site.clear()
-										for k in A[_D][_H]:l=Site.objects.get(id=k);B.site.add(l)
+										for r in A[_E][_F]:s=Site.objects.get(id=r);B.site.add(s)
 										B.save()
-							if Z:
-								if H in A[_A]:C.objects.filter(slug=A[_A][H]).update(slug=A[_A][H],created_at=A[_B][P])
-								elif F in A[_A]:C.objects.filter(judul_seo=A[_A][F]).update(judul_seo=A[_A][F],created_at=A[_B][P])
-								elif I in A[_A]:C.objects.filter(nama_seo=A[_A][I]).update(nama_seo=A[_A][I],created_at=A[_B][P])
-								else:C.objects.filter(id=A[_A][_C]).update(created_at=A[_B][P])
-							V+=1
-			J.info(_O)
-	@transaction.atomic
-	def update_data_by_site_old(self,site_group):
-		W='-----';M=site_group;J=self;N=os.path.join(J.site_list,M);K=[A for A in os.listdir(N)if os.path.isfile(os.path.join(N,A))]
-		for F in J.model_list:
-			for O in range(len(K)):
-				D=K[O].split('_');G=D[len(D)-1];G=G.split('.')[:1];G=G[0];D.pop(len(D)-1);P=0;Q='_'.join(D);
-				if F==Q:
-					f=None
-					with open(os.path.join(J.site_list,M,K[O]),_E)as X:R=json.load(X)
-					L=1;Y=len(R)
-					for A in R:
-						C=apps.get_model('opd',F);
-						if C:
-							S=_F;T=_F;U=_F
-							for H in C._meta.get_fields():
-								if H.many_to_many:
-									if H.name==_J:S=_G
-									elif H.name==_K:T=_G
-									elif H.name==_L:U=_G
-							if F==_N:
-								Z=menu.objects.filter(id=A[_B][_M])
-								if not Z:P+=1;L+=1;continue
-							a=L/Y;
-							if _I in A[_B]:
-								I=photo.objects.filter(id=A[_B][_I])
-								if I:B,b=C.objects.update_or_create(id=A[_A][_C],defaults=A[_B])
-								else:pass
-							else:B,b=C.objects.update_or_create(id=A[_A][_C],defaults=A[_B])
-							if S:
-								B.tags.clear()
-								for E in A[_D][_J]:c=tags.objects.get(id=E);B.tags.add(c)
-								B.save()
-							if T:
-								B.photo.clear();V=_F
-								for E in A[_D][_K]:
-									I=photo.objects.filter(id=E)
-									if I:d=I.get();B.photo.add(d);V=_G
-								if V:B.save()
-							if U:
-								B.admin.clear()
-								for E in A[_D][_L]:e=User.objects.get(id=E);B.admin.add(e)
-								B.save()
-							L+=1
-	def handle(A,*G,**D):
-		C='\n';A.info('Begin get site data');B=D[_Q];E=input('Confirm DB Name: ');F=settings.DATABASES
-		if E==F['default']['NAME']:A.update_site_model(B);A.info(C);A.update_user_model(B);A.info(C);A.update_kategori_model(B);A.info(C);A.update_tags_model(B);A.info(C);A.update_menu_model(B);A.info(C);A.update_photo_model(B);A.info(C);A.update_data_by_site(B);A.info(C)
-		else:A.info('db_name NOT MATCH:')
-		A.info('End get site data')
+							if c:
+								if F in A[_A]:C.objects.filter(slug=A[_A][F]).update(slug=A[_A][F],created_at=A[_B][R])
+								elif E in A[_A]:C.objects.filter(judul_seo=A[_A][E]).update(judul_seo=A[_A][E],created_at=A[_B][R])
+								elif G in A[_A]:C.objects.filter(nama_seo=A[_A][G]).update(nama_seo=A[_A][G],created_at=A[_B][R])
+								else:C.objects.filter(id=A[_A][_C]).update(created_at=A[_B][R])
+							Y+=1
+					H.info('\n')
+			H.info('\nSKIP '+str(W))
+	def handle(A,*F,**C):
+		A.info('Begin get site data\n');B=C[_I];D=input('Confirm DB Name: ');E=settings.DATABASES
+		if D==E['default']['NAME']:A.update_site_model(B);A.update_user_model(B);A.update_kategori_model(B);A.update_tags_model(B);A.update_menu_model(B);A.update_photo_model(B);A.update_data_by_site(B)
+		else:A.info('\ndb_name NOT MATCH:')
+		A.info('\nEnd get site data\n')
